b8b00c0909bbb098d19f49ca3b1630693d55c770
Balša Bulatović <63477276+BulatovicBalsa@users.noreply.github.com>
2023-06-20 04:23:07 +0200

    Update tabele.sql

--- tabele.sql
+++ tabele.sql
@@ -241,20 +241,14 @@ create table account_journal (
   CONSTRAINT operation_ch CHECK (operation in ('insert', 'update', 'delete'))
 );
 
-select champion_id, count(*) as ukupno_chroma from chroma group by champion_id;
 create or replace view champion_lookup as 
-select champion_moniker, ukupno_skinova, ukupno_chroma from champion natural join (select champion_id, count(*) ukupno_skinova from skin group by champion_id)
-left inner join (select champion_id as ccid, count(*) as ukupno_chroma from chroma group by champion_id) on champion_id = ccid order by champion_moniker;
+select champion_moniker, ukupno_skinova, ukupno_chroma, nvl(prosek_dugackih_emota, 0) as prosek_dugackih
+from champion natural join (select champion_id, count(*) ukupno_skinova from skin group by champion_id)
+left outer join (select champion_id as ccid, count(*) as ukupno_chroma from chroma group by champion_id) on champion_id = ccid
+left outer join (select champion_owner, round(avg(emote_duration), 2) as prosek_dugackih_emota 
+from emote where emote_duration > 4 group by champion_owner)
+on champion_id = champion_owner
+order by champion_moniker;
 
 select * from champion_lookup;
-select * from skin where champion_id = (select champion_id from champion where champion_moniker = 'Ashe');
-select * from skin natural join (select * from champion where champion_moniker = 'Ashe');
-
-select * from champion natural join (select champion_id, count(*) ukupno_skinova from skin group by champion_id)
-natural join (select champion_id, count(*) as ukupno_chroma from chroma group by champion_id) order by champion_moniker;
-
-select * from (select * from champion where champion_id = 15) natural join skin
-
-
-
 
